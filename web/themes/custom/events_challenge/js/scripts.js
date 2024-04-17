b00bdd09f6505d57c8db6df463e42a9ae32e9289
Vahan <vahan.margaryan@partners.neste.com>
2024-04-17 18:09:19 +0400

    Added new events theme

---
+++ web/themes/custom/events_challenge/js/scripts.js
@@ -0,0 +1,7 @@
+(function ($, Drupal) {
+  Drupal.behaviors.eventsChallengeBehavior = {
+    attach: function (context, settings) {
+      console.log('Events Challenge theme JavaScript works!');
+    }
+  };
+})(jQuery, Drupal);
